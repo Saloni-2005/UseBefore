006cab7b765d446f38c5aa2ec6f485364242f867
Saloni-2005 <chaudharysaloni2510@gmail.com>
2025-04-10 22:52:25 +0530

    Modifications on home-page of Houses

--- app/src/main/java/com/example/usebefore/view/HousesHomePage.kt
+++ app/src/main/java/com/example/usebefore/view/HousesHomePage.kt
@@ -12,10 +12,6 @@ class HousesHomePage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_houses_home_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
